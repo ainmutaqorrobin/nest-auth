9f92d96ffdfbf42597f6ee46f57f7a42c4962902
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-12-14 09:41:42 +0800

    wip: add user resources

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  avatarUrl: string;
+
+  @Column()
+  createdAt: string;
+
+  @Column()
+  password: string;
+}
